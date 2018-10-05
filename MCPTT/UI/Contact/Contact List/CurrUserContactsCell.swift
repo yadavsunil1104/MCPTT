2c0f078af559063cb4a5b3485862fd30cbe2f715
Raju Maramulla <raju@rajus-mbp.symphonyteleca.com>
2018-10-05 13:18:33 +0530

    Added Contact list UI screen

---
+++ MCPTT/UI/Contact/Contact List/CurrUserContactsCell.swift
@@ -0,0 +1,16 @@
+//
+//  CurrUserContactsCell.swift
+//  MCPTTv2
+//
+//  Created by Raju Maramulla on 04/10/18.
+//  Copyright © 2018 Raju Maramulla. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+class CurrUserContactsCell : UICollectionViewCell {
+    
+    @IBOutlet weak var currUserContactName: UILabel!
+    @IBOutlet weak var currUserContactMCID: UILabel!
+}
---
+++ MCPTT/UI/Contact/Contact List/CurrUserGroupsCell.swift
@@ -0,0 +1,15 @@
+//
+//  CurrUserGroupsCell.swift
+//  MCPTTv2
+//
+//  Created by Raju Maramulla on 04/10/18.
+//  Copyright © 2018 Raju Maramulla. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+class CurrUserGroupsCell : UICollectionViewCell {
+    
+    @IBOutlet weak var currUserGroupName: UILabel!
+}
